7f8cf5789cf8c51cd8972e464f3caf798c62ddb0
Jaroslaw-super1 <Jarafan3@gmail.com>
2025-10-25 19:57:38 +0500

    new function

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 # include <iostream>
 
+bool is_Pyth (unsigned a, unsigned b, unsigned c);
+
 int main () {
     using u_t = unsigned;
     u_t a = 0;
-    std::cin >> a;
+    u_t a = 0, b = 0, c = 0;
     size_t count = 0;
-    
+
 
     //std::cin >> a;
     while (std::cin >> a) {
         count++;
+        count += is_Pyth(a, b, c);
     }
 
     if (std::cin.eof()) {
-        std::cout << 1;
         std::cout << count;
         std::cout << "\n";
     } else if (std::cin.fail()) {
