bf857bda8d106c12993ff2abc3865546cf6271f3
Jaroslaw-super1 <Jarafan3@gmail.com>
2025-10-25 19:52:20 +0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+bool is_Pyth (unsigned a, unsigned b, unsigned c);
+
+int main () {
+    using u_t = unsigned;
+    u_t a = 0;
+    u_t a = 0;
+
+    std::cin >> a;
+
+    std::cout << 1 << "\n";
+}
